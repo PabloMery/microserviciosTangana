00bff5469a2936a7b0b6b67f10098fe546740bf0
Pablo <Pablomery2002@gmail.com>
2025-11-10 17:46:01 -0300

    Spring base

---
+++ src/main/java/com/Tangana/microserviciosTangana/MicroserviciosTanganaApplication.java
@@ -0,0 +1,13 @@
+package com.Tangana.microserviciosTangana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosTanganaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosTanganaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tangana/microserviciosTangana/MicroserviciosTanganaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tangana.microserviciosTangana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosTanganaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
